837fc152d253a01a488a3028321029ae5de877a5
Fernando Diaz Laclaustra <f.diazlaclaustra@umlaud.es>
2017-10-12 15:06:20 +0200

    Implementa clase principal 'LocalStreamer'.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = {};
+/**
+ * Module dependencies.
+ */
+var LocalStreamer = require('./lib/local-streamer');
+
+
+/**
+ * Expose `LocalStreamer` directly from package.
+ */
+exports = module.exports = LocalStreamer;
